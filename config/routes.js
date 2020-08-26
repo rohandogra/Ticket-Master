fadcf2b9e43bcd63a066a8908585fef3293a371b
unknown <dogra.rohan25@gmail.com>
2020-08-27 00:07:44 +0530

    make it better

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
