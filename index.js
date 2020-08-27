e3fe08bf5db716d33b0a09e34170ab2419f9defa
unknown <dogra.rohan25@gmail.com>
2020-08-27 10:06:29 +0530

    make it better

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ configDB();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "client/build/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
 });
 
 app.use("/api/user", router);
