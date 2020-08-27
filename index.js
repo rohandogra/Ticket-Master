a053e9a44b9b53d552a4b5270c5558fc62e8f21c
unknown <dogra.rohan25@gmail.com>
2020-08-27 10:17:19 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 5000;
 const path = require("path");
 require("dotenv").config();
 const configDB = require("./config/database");
 const router = require("./config/routes");
+const client = require("./client/build");
 
 const cors = require("cors");
 configDB();
@@ -12,10 +13,11 @@ configDB();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
+//* Handles any requests that don't match the ones above
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.use("/api/user", router);
