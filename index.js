c821356f7f7088345d08b4592b0c2c5d09d9841b
unknown <dogra.rohan25@gmail.com>
2020-08-27 10:27:59 +0530

    deployment

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require("path");
 require("dotenv").config();
 const configDB = require("./config/database");
 const router = require("./config/routes");
-const client = require("./client/build");
 
 const cors = require("cors");
 configDB();
@@ -15,12 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.use("/api/user", router);
+
 //* Handles any requests that don't match the ones above
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-
-app.use("/api/user", router);
 
 app.listen(port, () => {
   console.log("Listing node server on port number", port);
